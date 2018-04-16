fd8bd6ddb78fde28fccae6bebf0a9375680156f3
Vincent <luoxiang@megvii.com>
2018-04-16 12:38:19 +0800

    finish

--- app/src/main/java/com/vincent/luoxiang/smsmanager/MainActivity.java
+++ app/src/main/java/com/vincent/luoxiang/smsmanager/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Random;
+
 public class MainActivity
         extends AppCompatActivity
         implements View.OnClickListener
@@ -18,11 +20,20 @@ public class MainActivity
 
     private static final int ONE_DAY = 1000 * 60 * 60 * 24;
     //【铁路客服】订单EF18430720,罗享您已购4月15日G6533次5车2A号,广州南站14:55开。
-    private static final String SMS_TMPLATE = "【铁路客服】订单EF%d,%s您已购%s月%s日%s次%s车%s,%s站%s开。";
+    private static final String SMS_TMPLATE = "【铁路客服】订单EF%s,%s您已购%s月%s日%s次%s车%s,%s站%s开。";
     private EditText mEtAdd;
     private EditText mEtContent;
     private View mSend;
     private  boolean mModeNormal;
+    private EditText mName;
+    private EditText mMonth;
+    private EditText mDay;
+    private EditText mNum;
+    private EditText mCar;
+    private EditText mSeat;
+    private EditText mStart;
+    private EditText mTime;
+    private View mLl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +53,17 @@ public class MainActivity
     public void initView() {
         mEtAdd = (EditText) findViewById(R.id.et_address);
         mEtContent = (EditText)findViewById(R.id.et_content);
+        mName = (EditText) findViewById(R.id.et_name);
+        mMonth = (EditText) findViewById(R.id.et_month);
+        mDay = (EditText) findViewById(R.id.et_day);
+        mNum = (EditText) findViewById(R.id.et_num);
+        mCar = (EditText) findViewById(R.id.et_car);
+        mSeat = (EditText) findViewById(R.id.et_seat);
+        mStart = (EditText) findViewById(R.id.et_start);
+        mTime = (EditText) findViewById(R.id.et_time);
+
         mSend = findViewById(R.id.btn_send);
+        mLl = findViewById(R.id.ll_input_tamplate);
     }
 
     /**
@@ -61,9 +82,29 @@ public class MainActivity
 
     @Override
     public void onClick(View view) {
-        if (TextUtils.isEmpty(mEtAdd.getText().toString()) || TextUtils.isEmpty(mEtContent.getText().toString())){
-            return;
+        if (!mModeNormal){
+            if (TextUtils.isEmpty(mEtAdd.getText().toString()) ||
+                    TextUtils.isEmpty(mEtContent.getText().toString())){
+                return;
+            }
+        }else {
+            if (TextUtils.isEmpty(mEtAdd.getText().toString()) ||
+                    TextUtils.isEmpty(mName.getText().toString()) ||
+                    TextUtils.isEmpty(mMonth.getText().toString()) ||
+                    TextUtils.isEmpty(mDay.getText().toString()) ||
+                    TextUtils.isEmpty(mNum.getText().toString()) ||
+                    TextUtils.isEmpty(mCar.getText().toString()) ||
+                    TextUtils.isEmpty(mSeat.getText().toString()) ||
+                    TextUtils.isEmpty(mStart.getText().toString()) ||
+                    TextUtils.isEmpty(mTime.getText().toString())
+                    ){
+
+
+
+                return;
+            }
         }
+
         // 需要 使用 后门程序 --- 需要 使用 contentResolver
         ContentResolver resolver = getContentResolver();
 
@@ -76,7 +117,28 @@ public class MainActivity
         values.put("address", mEtAdd.getText().toString());
         values.put("date", System.currentTimeMillis() - ONE_DAY);
         values.put("type", "1");
-        values.put("body", mEtContent.getText().toString());
+        String content = "";
+        if (!mModeNormal){
+            content = mEtContent.getText().toString();
+        }else {
+            Random random = new Random();
+            StringBuffer str = new StringBuffer();
+            for(int i=0;i<8;i++){
+                str.append(random.nextInt(10));
+            }
+            content = String.format(SMS_TMPLATE , str.toString() ,
+                                    mName.getText().toString() ,
+                                    mMonth.getText().toString() ,
+                                    mDay.getText().toString() ,
+                                    mNum.getText().toString() ,
+                                    mCar.getText().toString() ,
+                                    mSeat.getText().toString() ,
+                                    mStart.getText().toString() ,
+                                    mTime.getText().toString()
+            );
+        }
+
+        values.put("body", content);
 
         resolver.insert(uri, values);
     }
@@ -90,7 +152,14 @@ public class MainActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.change_mode){
-
+            mModeNormal = !mModeNormal;
+            if (mModeNormal){
+                mLl.setVisibility(View.VISIBLE);
+                mEtContent.setVisibility(View.GONE);
+            }else {
+                mLl.setVisibility(View.GONE);
+                mEtContent.setVisibility(View.VISIBLE);
+            }
         }
         return super.onOptionsItemSelected(item);
     }
